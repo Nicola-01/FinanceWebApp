e82e3e091b04a93b20517dfc82ee1e785f329617
Nicola <nickbusato0101@gmail.com>
2026-04-02 23:09:34 +0200

    update

--- frontend/src/utils/PWAContext.tsx
+++ frontend/src/utils/PWAContext.tsx
@@ -16,12 +16,18 @@ export const PWAProvider: React.FC<{ children: React.ReactNode }> = ({ children 
     const [installPrompt, setInstallPrompt] = useState<any>(null);
 
     useEffect(() => {
+        // Controlliamo se per caso è scattato prima che React montasse il componente
+        if ((window as any)._pwaInstallPrompt) {
+            setInstallPrompt((window as any)._pwaInstallPrompt);
+        }
+
         const handler = (e: any) => {
             console.log("PWA beforeinstallprompt event captured");
             // Prevent the mini-infobar from appearing on mobile
             e.preventDefault();
             // Stash the event so it can be triggered later.
             setInstallPrompt(e);
+            (window as any)._pwaInstallPrompt = e;
         };
 
         window.addEventListener('beforeinstallprompt', handler);
